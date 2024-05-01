775dd11d7e0b1eccd5fbcb2a977e4d894abc5203
Kuldeep Sen <kuldeepsen134>
2024-05-01 22:32:12 +0530

    Home page two section layout

--- src/images/index.js
+++ src/images/index.js
@@ -1,5 +1,9 @@
 import logo from "./logo.png";
 import Saas1 from './SaaS-1.png'
 import Saas3 from './Shape3.png'
+import phone from './phone.png'
+import locationIcon from './location.png'
+import email from './email.png'
+import imageOne from './image-one.png'
 
-export { logo, Saas1, Saas3 }
+export { logo, Saas1, Saas3, phone, locationIcon, email, imageOne }
--- src/pages/about/ContactUs.jsx
+++ src/pages/about/ContactUs.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { email, locationIcon, phone } from "../../images";
 
 const ContactUs = () => {
   return (
@@ -8,84 +9,87 @@ const ContactUs = () => {
           <h2>Contact Us</h2>
         </div>
       </section>
-      <div className="container py-5">
-        <div className="row mx-0 justify-content-around position-relative">
-          <div className="col-sm-5">
-            <div className="contact-detail">
-              <div className="mb-4">
-                <i></i>
-                <h3>+91 - 8482925926</h3>
-              </div>
-              <div className="mb-4">
-                <i></i>
-                <h3>+info@nakshatratechnohub.com</h3>
-              </div>
-              <div className="mb-4">
-                <i></i>
-                <h3>Registered office</h3>
-                <p>+F-89, MIDC, AMBAD, NASHIK - 422010, MAHARASHTRA</p>
-              </div>
-              <div className="mb-4">
-                <i></i>
-                <h3>Factory</h3>
-                <p>+F-89, MIDC, AMBAD, NASHIK - 422010, MAHARASHTRA</p>
+
+      <section>
+        <div className="container">
+          <div className="row">
+            <div className="col-sm-5">
+              <div className="contact-detail">
+                <div className="mb-4">
+                  <img src={phone} alt="" />
+                  <h4>+91 - 8482925926</h4>
+                </div>
+                <div className="mb-4">
+                  <img src={email} alt="" />
+                  <h4>+info@nakshatratechnohub.com</h4>
+                </div>
+                <div className="mb-4">
+                  <img src={locationIcon} alt="" />
+                  <h4>Registered office</h4>
+                  <p>+F-89, MIDC, AMBAD, NASHIK - 422010, MAHARASHTRA</p>
+                </div>
+                <div className="mb-4">
+                  <img src={locationIcon} alt="" />
+                  <h4>Factory</h4>
+                  <p>+F-89, MIDC, AMBAD, NASHIK - 422010, MAHARASHTRA</p>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="col-sm-6 position-relative">
-            <form className="contact-detail-form">
-              <div>
-                <input
-                  type="text"
-                  className="form-control mb-4"
-                  placeholder="Name"
-                  name=""
-                  id=""
-                />
-              </div>
-              <div>
-                <input
-                  type="text"
-                  className="form-control mb-4"
-                  placeholder="email"
-                  name=""
-                  id=""
-                />
-              </div>
-              <div>
-                <input
-                  type="text"
-                  className="form-control mb-4"
-                  placeholder="phone"
-                  name=""
-                  id=""
-                />
-              </div>
-              <div>
-                <input
-                  type="text"
-                  className="form-control mb-4"
-                  placeholder="subject"
-                  name=""
-                  id=""
-                />
-              </div>
-              <div>
-                <textarea
-                  name=""
-                  placeholder="message"
-                  id=""
-                  cols="30"
-                  rows="10"
-                ></textarea>
-              </div>
-              <div className="border mt-4  text-center p-2 rounded-pill text-decoration-none text-success border-success">
-                <button className="btn ">Send</button>
-              </div>
-            </form>
+            <div className="col-sm-6 position-relative">
+              <form className="contact-detail-form">
+                <div>
+                  <input
+                    type="text"
+                    className="form-control mb-4"
+                    placeholder="Name"
+                    name=""
+                    id=""
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    className="form-control mb-4"
+                    placeholder="email"
+                    name=""
+                    id=""
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    className="form-control mb-4"
+                    placeholder="phone"
+                    name=""
+                    id=""
+                  />
+                </div>
+                <div>
+                  <input
+                    type="text"
+                    className="form-control mb-4"
+                    placeholder="subject"
+                    name=""
+                    id=""
+                  />
+                </div>
+                <div>
+                  <textarea
+                    name=""
+                    placeholder="message"
+                    id=""
+                    cols="30"
+                    rows="10"
+                  ></textarea>
+                </div>
+                <div className="border mt-4  text-center p-2 rounded-pill text-decoration-none text-success border-success">
+                  <button className="btn ">Send</button>
+                </div>
+              </form>
+            </div>
           </div>
         </div>
-      </div>
+      </section>
     </>
   );
 };
