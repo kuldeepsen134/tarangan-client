ace22406106488c06d60db2aa6bd18e0d19cba5f
Parkhya.itpark <parkhya.itparkh@parkhya.com>
2024-05-02 10:45:15 +0530

    Created two sections

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -42,81 +42,109 @@ const HomePage = () => {
       </section>
 
       <section>
-        <div className="container">
-          <div className="row row-cols-1 row-cols-md-2 row-cols-lg-5">
-            <div className="col">
-              <a href="">
-                <div className="card">
-                  <div className="card-image">
-                    <img src={imageOne} alt="" className="img-fluid" />
+        <div className="tarangan2">
+          <div className="container">
+            <div className="row row-cols-1 row-cols-md-2 row-cols-lg-5">
+              <div className="col">
+                <a href="">
+                  <div className="card">
+                    <div className="card-image rounded-circle">
+                      <img src={imageOne} alt="" className="img-fluid" />
+                    </div>
+                    <div className="card-body">
+                      <p className="mb-0">Nakshatra Technohub</p>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    <p className="mb-0">
-                      Nakshatra Technohub
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
+                </a>
+              </div>
 
-            <div className="col">
-              <a href="">
-                <div className="card">
-                  <div className="card-image">
-                    <img src={imageOne} alt="" className="img-fluid" />
+              <div className="col">
+                <a href="">
+                  <div className="card">
+                    <div className="card-image">
+                      <img src={imageOne} alt="" className="img-fluid" />
+                    </div>
+                    <div className="card-body">
+                      <p className="mb-0">Nakshatra Technohub</p>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    <p className="mb-0">
-                      Nakshatra Technohub
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
+                </a>
+              </div>
 
-            <div className="col">
-              <a href="">
-                <div className="card">
-                  <div className="card-image">
-                    <img src={imageOne} alt="" className="img-fluid" />
+              <div className="col">
+                <a href="">
+                  <div className="card">
+                    <div className="card-image">
+                      <img src={imageOne} alt="" className="img-fluid" />
+                    </div>
+                    <div className="card-body">
+                      <p className="mb-0">Nakshatra Technohub</p>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    <p className="mb-0">
-                      Nakshatra Technohub
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
+                </a>
+              </div>
 
-            <div className="col">
-              <a href="">
-                <div className="card">
-                  <div className="card-image">
-                    <img src={imageOne} alt="" className="img-fluid" />
+              <div className="col">
+                <a href="">
+                  <div className="card">
+                    <div className="card-image">
+                      <img src={imageOne} alt="" className="img-fluid" />
+                    </div>
+                    <div className="card-body">
+                      <p className="mb-0">Nakshatra Technohub</p>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    <p className="mb-0">
-                      Nakshatra Technohub
-                    </p>
-                  </div>
-                </div>
-              </a>
-            </div>
+                </a>
+              </div>
 
-            <div className="col">
-              <a href="">
-                <div className="card">
-                  <div className="card-image">
-                    <img src={imageOne} alt="" className="img-fluid" />
+              <div className="col">
+                <a href="">
+                  <div className="card">
+                    <div className="card-image">
+                      <img src={imageOne} alt="" className="img-fluid" />
+                    </div>
+                    <div className="card-body">
+                      <p className="mb-0">Nakshatra Technohub</p>
+                    </div>
                   </div>
-                  <div className="card-body">
-                    <p className="mb-0">
-                      Nakshatra Technohub
-                    </p>
-                  </div>
-                </div>
-              </a>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section>
+        <div className="think-plan">
+          <div className="container">
+            <div className="text-center">
+              <h1 className="">Think | Plan | Execute</h1>
+              <p>
+                Nakshatra group stands united since 2012 and with our tremendous
+                efforts and world-class services we have achieved great success
+                with quality performance. Our every enterprise has worldwide
+                presence as the name Nakshatra itself defines the unity of
+                universe's heavenly bodies, considering the same all our
+                businesses work in a collaborative manner and become part of
+                each others growth.
+              </p>
+            </div>
+            <div className="row">
+              <div className="col-3 text-center text-danger">
+                <h2>10 +</h2>
+                <p>Years of Experience</p>
+              </div>
+              <div className="col-3 text-center text-warning">
+                <h2>10 +</h2>
+                <p>Years of Experience</p>
+              </div>
+              <div className="col-3 text-center text-success">
+                <h2>10 +</h2>
+                <p>Years of Experience</p>
+              </div>
+              <div className="col-3 text-center text-primary">
+                <h2>10 +</h2>
+                <p>Years of Experience</p>
+              </div>
             </div>
           </div>
         </div>
